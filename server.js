71a93c51da01b7ec54076814564c3752fb62f029
Ahmed Abdullah <ahmed@sudofy.com>
2020-04-01 11:40:13 +0500

    cors enabled

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 const iota = require('./routes/iota');
 
 const app = express();
 const port = 5000;
 
 app.use(bodyParser.json());
+
+app.use(cors());
 
 app.use('/api/iota',iota);
 
