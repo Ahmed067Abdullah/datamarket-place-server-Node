5e53fcc218e7cb3cc7f2d12c9e32bcc050fd801c
Ahmed Abdullah <ahmed@sudofy.com>
2020-04-01 19:17:30 +0500

    config file removed from gitignore and env variables created

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  firebaseEndPoint: process.env.firebaseEndPoint,
+  provider: 'https://nodes.devnet.iota.org',
+  iotaApiVersion: 1,
+  defaultPrice: 10,
+  secretKey: process.env.secretKey
+};
