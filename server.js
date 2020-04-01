0582ce41a2fc19fdcec3d70b6bd49d19279f1698
Ahmed Abdullah <ahmed@sudofy.com>
2020-04-01 18:57:05 +0500

    port number chagned

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const iota = require('./routes/iota');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
