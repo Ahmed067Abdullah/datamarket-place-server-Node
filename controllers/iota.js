beb20fb1d98f7c78433a0641ddfd9284992aed01
Ahmed Abdullah <ahmed@sudofy.com>
2020-04-02 11:56:19 +0500

    heasd

--- controllers/iota.js
+++ controllers/iota.js
@@ -8,97 +8,108 @@ const get = (req, res) => {
   res.send({ msg: 'Hello World' });
 };
 
-const purchaseStream = async (req, res) => {
+const purchaseStream = (req, res) => {
   // Check Fields
-  const packet = req.body;
-  if (!packet || !packet.userId || !packet.deviceId || !packet.seed) {
-    console.error("purchaseStream failed. Packet: ", packet);
-    return res.status(400).json({ error: "Malformed Request", packet });
-  }
-
-  try {
-    const { firebaseEndPoint, provider, iotaApiVersion, defaultPrice, secretKey } = config;
-    const device = (await axios.get(`${firebaseEndPoint}/device?deviceId=${packet.deviceId}`)).data;
-    let price = defaultPrice;
-    if (device) {
-      if ((device.buyers || []).includes(packet.userId)) {
-        return res.status(403).json({ error: "Device already bought" });
-      }
-      if (device.price) {
-        price = Number(device.price);
-      } else if (device.value) {
-        price = Number(device.value);
-      }
-    } else {
-      return res.status(404).json({ error: `Device doesn't exist` });
-    }
-
-    const { getBalances, sendTrytes, getLatestInclusion, getNewAddress, prepareTransfers } = composeAPI({ provider });
-
-    // Balance validation code, open for production, commented for development
-    // const allAddresses = await getNewAddress(packet.seed, { returnAll: true });
-    // const { balances } = await getBalances(allAddresses, 10);
-    // const totalBalance = balances.reduce((el, sum) => sum + el, 0);
-    // if (price > totalBalance) {
-    //   return res.status(404).json({ error: `Insufficient balanc, ${totalBalance}` });
+  return new Promise(async (resolve, reject) => {
+    setTimeout(() => {
+      res.status(303).json({ success: true });
+    }, 10000)
+    // const packet = req.body;
+    // if (!packet || !packet.userId || !packet.deviceId || !packet.seed) {
+    //   console.error("purchaseStream failed. Packet: ", packet);
+    //   res.status(400).json({ error: "Malformed Request", packet });
+    //   resolve();
     // }
 
-    const security = 2;
+    // try {
+    //   const { firebaseEndPoint, provider, iotaApiVersion, defaultPrice, secretKey } = config;
+    //   const device = (await axios.get(`${firebaseEndPoint}/device?deviceId=${packet.deviceId}`)).data;
+    //   let price = defaultPrice;
+    //   if (device) {
+    //     if ((device.buyers || []).includes(packet.userId)) {
+    //       res.status(403).json({ error: "Device already bought" });
+    //       resolve();
+    //     }
+    //     if (device.price) {
+    //       price = Number(device.price);
+    //     } else if (device.value) {
+    //       price = Number(device.value);
+    //     }
+    //   } else {
+    //     res.status(404).json({ error: `Device doesn't exist` });
+    //     resolve();
+    //   }
 
-    // Depth or how far to go for tip selection entry point
-    const depth = 5
+    //   const { getBalances, sendTrytes, getLatestInclusion, getNewAddress, prepareTransfers } = composeAPI({ provider });
 
-    // Difficulty of Proof-of-Work required to attach transaction to tangle.
-    // Minimum value on mainnet & spamnet is `14`, `9` on devnet and other testnets.
-    const minWeightMagnitude = 9
+    //   // Balance validation code, open for production, commented for development
+    //   // const allAddresses = await getNewAddress(packet.seed, { returnAll: true });
+    //   // const { balances } = await getBalances(allAddresses, 10);
+    //   // const totalBalance = balances.reduce((el, sum) => sum + el, 0);
+    //   // if (price > totalBalance) {
+    //   //   return res.status(404).json({ error: `Insufficient balanc, ${totalBalance}` });
+    //   // }
 
-    const transfers = [{ address: device.address, value: 0 }];
-    const trytes = await prepareTransfers(packet.seed, transfers);
-    const transactions = await sendTrytes(trytes, depth, minWeightMagnitude);
-    console.log(transactions)
-    const hashes = transactions.map(transaction => transaction.hash);
+    //   const security = 2;
 
-    let retries = 0;
-    while (retries++ < 20) {
-      console.log(retries)
-      const statuses = await getLatestInclusion(hashes)
-      if (statuses.filter(status => status).length === 4) break;
-      await new Promise(resolved => setTimeout(resolved, 10000));
-    }
+    //   // Depth or how far to go for tip selection entry point
+    //   const depth = 5
 
-    if (transactions) {
-      const hashes =
-        transactions && transactions.map(transaction => transaction.hash);
+    //   // Difficulty of Proof-of-Work required to attach transaction to tangle.
+    //   // Minimum value on mainnet & spamnet is `14`, `9` on devnet and other testnets.
+    //   const minWeightMagnitude = 9
 
-      // Find TX on network and parse
-      const bundle = await findTx(hashes, provider, iotaApiVersion);
+    //   const transfers = [{ address: device.address, value: 0 }];
+    //   const trytes = await prepareTransfers(packet.seed, transfers);
+    //   const transactions = await sendTrytes(trytes, depth, minWeightMagnitude);
+    //   console.log(transactions)
+    //   const hashes = transactions.map(transaction => transaction.hash);
 
-      // Make sure TX is valid
-      if (!validateBundleSignatures(bundle)) {
-        console.error(
-          "purchaseStream failed. Transaction is invalid for: ",
-          bundle
-        );
-        res.status(403).json({ error: "Transaction is Invalid" });
-      }
+    //   let retries = 0;
+    //   while (retries++ < 20) {
+    //     console.log(retries)
+    //     const statuses = await getLatestInclusion(hashes)
+    //     if (statuses.filter(status => status).length === 4) break;
+    //     await new Promise(resolved => setTimeout(resolved, 10000));
+    //   }
 
-      const payload = {
-        userId: packet.userId,
-        deviceId: packet.deviceId,
-        secretKey
-      };
-      // await axios.post(`${firebaseEndPoint}/boughtDevice`, payload)
-      console.log('here');
-      return res.write(JSON.stringify({ success: true }));
-      // return res.json({ success: true });
-    }
-    return res.status(403).json({
-      error: "Purchase failed. Insufficient balance of out of sync"
-    });
-  } catch (e) {
-    console.error("purchaseData failed. Error: ", e);
-    return res.status(403).json({ error: e.message });
-  }
+    //   if (transactions) {
+    //     const hashes =
+    //       transactions && transactions.map(transaction => transaction.hash);
+
+    //     // Find TX on network and parse
+    //     const bundle = await findTx(hashes, provider, iotaApiVersion);
+
+    //     // Make sure TX is valid
+    //     if (!validateBundleSignatures(bundle)) {
+    //       console.error(
+    //         "purchaseStream failed. Transaction is invalid for: ",
+    //         bundle
+    //       );
+    //       res.status(403).json({ error: "Transaction is Invalid" });
+    //       resolve();
+    //     }
+
+    //     const payload = {
+    //       userId: packet.userId,
+    //       deviceId: packet.deviceId,
+    //       secretKey
+    //     };
+    //     // await axios.post(`${firebaseEndPoint}/boughtDevice`, payload)
+    //     console.log('here');
+    //     res.json({ success: true });
+    //     resolve();
+    //   }
+    //   res.status(403).json({
+    //     error: "Purchase failed. Insufficient balance of out of sync"
+    //   });
+    //   resolve();
+    // } catch (e) {
+    //   console.error("purchaseData failed. Error: ", e);
+    //   res.status(403).json({ error: e.message });
+    //   resolve();
+    // }
+  });
 }
 
 const findTx = (hashes, provider, iotaApiVersion) => {
--- routes/iota.js
+++ routes/iota.js
@@ -5,61 +5,21 @@ const DelayedResponse = require('http-delayed-response');
 const router = express.Router();
 
 router.use(function (req, res, next) {
-  const space = ' ';
-  let isFinished = false;
-  let isDataSent = false;
-
-  // Only extend the timeout for API requests
+  // Only extend the timeout for purchaseStream
   if (!req.url.includes('/purchaseStream')) {
     next();
     return;
   }
-
-  res.once('finish', () => {
-    isFinished = true;
-  });
-
-  res.once('end', () => {
-    isFinished = true;
-  });
-
-  res.once('close', () => {
-    isFinished = true;
-  });
-
-  res.on('data', (data) => {
-    // Look for something other than our blank space to indicate that real
-    // data is now being sent back to the client.
-    console.log('ad', data);
-    if (data !== space) {
-      isDataSent = true;
-    }
-  });
-
-  const waitAndSend = () => {
-    setTimeout(() => {
-      // If the response hasn't finished and hasn't sent any data back....
-      if (!isFinished && !isDataSent) {
-        // Need to write the status code/headers if they haven't been sent yet.
-        if (!res.headersSent) {
-          res.writeHead(202);
-        }
-
-        res.write(space);
-
-        // Wait another 15 seconds
-        waitAndSend();
-      }
-    }, 15000);
-  };
-
-  waitAndSend();
-  next();
+  var delayed = new DelayedResponse(req, res);
+  delayed.wait();
+  var promise = iotaController.purchaseStream(req, res);
+  // will eventually end when the promise is fulfilled
+  delayed.end(promise);
 });
 
 router.get("/", iotaController.get);
 
-router.post("/purchaseStream", iotaController.purchaseStream);
+// router.post("/purchaseStream", iotaController.purchaseStream);
 
 
 module.exports = router;
