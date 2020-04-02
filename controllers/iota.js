76d7b19d525ee84d59209aefdcc4c09463a2aa06
Ahmed Abdullah <ahmed@sudofy.com>
2020-04-02 19:19:16 +0500

    dfdf

--- controllers/iota.js
+++ controllers/iota.js
@@ -8,109 +8,121 @@ const get = (req, res) => {
   res.send({ msg: 'Hello World' });
 };
 
-const purchaseStream = (req, res) => {
+const purchaseStream = async (req, res) => {
   // Check Fields
-  return new Promise(async (resolve, reject) => {
-    setTimeout(() => {
-      res.status(303).json({ success: true });
-      resolve();
-    }, 60000);
-    // const packet = req.body;
-    // if (!packet || !packet.userId || !packet.deviceId || !packet.seed) {
-    //   console.error("purchaseStream failed. Packet: ", packet);
-    //   res.status(400).json({ error: "Malformed Request", packet });
-    //   resolve();
+  const packet = req.body;
+  if (!packet || !packet.userId || !packet.deviceId || !packet.seed) {
+    console.error("purchaseStream failed. Packet: ", packet);
+    await setMessageToFirebase(packet.userId, packet.deviceId, 'Malformed Request, fill all field', false);
+    return res.status(400).json({ error: "Malformed Request", packet });
+  }
+
+  const { deviceId, userId, seed } = packet
+  try {
+    const { firebaseEndPoint, provider, iotaApiVersion, defaultPrice, secretKey } = config;
+    const device = (await axios.get(`${firebaseEndPoint}/device?deviceId=${deviceId}`)).data;
+    console.log(device)
+    let price = defaultPrice;
+    if (device) {
+      if ((device.buyers || []).includes(userId)) {
+        await setMessageToFirebase(userId, deviceId, 'Device already bought', false);
+        return res.status(403).json({ error: "Device already bought" });
+      }
+      if (device.price) {
+        price = Number(device.price);
+      } else if (device.value) {
+        price = Number(device.value);
+      }
+    } else {
+      await setMessageToFirebase(userId, deviceId, `Device doesn't exist`, false);
+      return res.status(404).json({ error: `Device doesn't exist` });
+    }
+
+    const { getBalances, sendTrytes, getLatestInclusion, getNewAddress, prepareTransfers } = composeAPI({ provider });
+
+    // Balance validation code, open for production, commented for development
+    // const allAddresses = await getNewAddress(packet.seed, { returnAll: true });
+    // const { balances } = await getBalances(allAddresses, 10);
+    // const totalBalance = balances.reduce((el, sum) => sum + el, 0);
+    // if (price > totalBalance) {
+    //   await setMessageToFirebase(userId, deviceId, `Insufficient balanc, ${totalBalance}`, false);
+    //   return res.status(404).json({ error: `Insufficient balanc, ${totalBalance}` });
     // }
 
-    // try {
-    //   const { firebaseEndPoint, provider, iotaApiVersion, defaultPrice, secretKey } = config;
-    //   const device = (await axios.get(`${firebaseEndPoint}/device?deviceId=${packet.deviceId}`)).data;
-    //   let price = defaultPrice;
-    //   if (device) {
-    //     if ((device.buyers || []).includes(packet.userId)) {
-    //       res.status(403).json({ error: "Device already bought" });
-    //       resolve();
-    //     }
-    //     if (device.price) {
-    //       price = Number(device.price);
-    //     } else if (device.value) {
-    //       price = Number(device.value);
-    //     }
-    //   } else {
-    //     res.status(404).json({ error: `Device doesn't exist` });
-    //     resolve();
-    //   }
+    const security = 2;
 
-    //   const { getBalances, sendTrytes, getLatestInclusion, getNewAddress, prepareTransfers } = composeAPI({ provider });
+    // Depth or how far to go for tip selection entry point
+    const depth = 5
 
-    //   // Balance validation code, open for production, commented for development
-    //   // const allAddresses = await getNewAddress(packet.seed, { returnAll: true });
-    //   // const { balances } = await getBalances(allAddresses, 10);
-    //   // const totalBalance = balances.reduce((el, sum) => sum + el, 0);
-    //   // if (price > totalBalance) {
-    //   //   return res.status(404).json({ error: `Insufficient balanc, ${totalBalance}` });
-    //   // }
+    // Difficulty of Proof-of-Work required to attach transaction to tangle.
+    // Minimum value on mainnet & spamnet is `14`, `9` on devnet and other testnets.
+    const minWeightMagnitude = 9
 
-    //   const security = 2;
+    const transfers = [{ address: device.address, value: 0 }];
+    const trytes = await prepareTransfers(seed, transfers);
+    const transactions = await sendTrytes(trytes, depth, minWeightMagnitude);
+    console.log(transactions)
+    const hashes = transactions.map(transaction => transaction.hash);
 
-    //   // Depth or how far to go for tip selection entry point
-    //   const depth = 5
+    let retries = 0;
+    while (retries++ < 20) {
+      console.log(retries)
+      const statuses = await getLatestInclusion(hashes)
+      if (statuses.filter(status => status).length === 4) break;
+      await new Promise(resolved => setTimeout(resolved, 10000));
+    }
 
-    //   // Difficulty of Proof-of-Work required to attach transaction to tangle.
-    //   // Minimum value on mainnet & spamnet is `14`, `9` on devnet and other testnets.
-    //   const minWeightMagnitude = 9
+    if (transactions) {
+      const hashes =
+        transactions && transactions.map(transaction => transaction.hash);
 
-    //   const transfers = [{ address: device.address, value: 0 }];
-    //   const trytes = await prepareTransfers(packet.seed, transfers);
-    //   const transactions = await sendTrytes(trytes, depth, minWeightMagnitude);
-    //   console.log(transactions)
-    //   const hashes = transactions.map(transaction => transaction.hash);
+      // Find TX on network and parse
+      const bundle = await findTx(hashes, provider, iotaApiVersion);
 
-    //   let retries = 0;
-    //   while (retries++ < 20) {
-    //     console.log(retries)
-    //     const statuses = await getLatestInclusion(hashes)
-    //     if (statuses.filter(status => status).length === 4) break;
-    //     await new Promise(resolved => setTimeout(resolved, 10000));
-    //   }
+      // Make sure TX is valid
+      if (!validateBundleSignatures(bundle)) {
+        console.error(
+          "purchaseStream failed. Transaction is invalid for: ",
+          bundle
+        );
+        await setMessageToFirebase(userId, deviceId, "Transaction is Invalid", false);
+        return res.status(403).json({ error: "Transaction is Invalid" });
+      }
 
-    //   if (transactions) {
-    //     const hashes =
-    //       transactions && transactions.map(transaction => transaction.hash);
+      const payload = {
+        userId,
+        deviceId,
+        secretKey
+      };
+      // await axios.post(`${firebaseEndPoint}/boughtDevice`, payload)
+      console.log('here');
+      await setMessageToFirebase(userId, deviceId, "Device bought successfully", true);
+      return res.json({ success: true });
+    }
+    await setMessageToFirebase(userId, deviceId, "Purchase failed. Insufficient balance of out of sync", false);
+    return res.status(403).json({
+      error: "Purchase failed. Insufficient balance of out of sync"
+    });
 
-    //     // Find TX on network and parse
-    //     const bundle = await findTx(hashes, provider, iotaApiVersion);
+  } catch (e) {
+    console.error("purchaseData failed. Error: ", e);
+    await setMessageToFirebase(userId, deviceId, e.message, false);
+    return res.status(403).json({ error: e.message });
+  }
+}
 
-    //     // Make sure TX is valid
-    //     if (!validateBundleSignatures(bundle)) {
-    //       console.error(
-    //         "purchaseStream failed. Transaction is invalid for: ",
-    //         bundle
-    //       );
-    //       res.status(403).json({ error: "Transaction is Invalid" });
-    //       resolve();
-    //     }
-
-    //     const payload = {
-    //       userId: packet.userId,
-    //       deviceId: packet.deviceId,
-    //       secretKey
-    //     };
-    //     // await axios.post(`${firebaseEndPoint}/boughtDevice`, payload)
-    //     console.log('here');
-    //     res.json({ success: true });
-    //     resolve();
-    //   }
-    //   res.status(403).json({
-    //     error: "Purchase failed. Insufficient balance of out of sync"
-    //   });
-    //   resolve();
-    // } catch (e) {
-    //   console.error("purchaseData failed. Error: ", e);
-    //   res.status(403).json({ error: e.message });
-    //   resolve();
-    // }
-  });
+const setMessageToFirebase = async (userId, deviceId, message, success) => {
+  const { firebaseEndPoint } = config;
+  const payload = {
+    userId, deviceId, message, success
+  };
+  console.log(payload)
+  try {
+    await axios.post(`${firebaseEndPoint}/setMessage`, payload);
+  }
+  catch (e) {
+    console.log(e.message);
+  }
 }
 
 const findTx = (hashes, provider, iotaApiVersion) => {
--- routes/iota.js
+++ routes/iota.js
@@ -4,22 +4,10 @@ const DelayedResponse = require('http-delayed-response');
 
 const router = express.Router();
 
-router.use(function (req, res, next) {
-  // Only extend the timeout for purchaseStream
-  if (!req.url.includes('/purchaseStream')) {
-    next();
-    return;
-  }
-  var delayed = new DelayedResponse(req, res);
-  delayed.wait();
-  var promise = iotaController.purchaseStream(req, res);
-  // will eventually end when the promise is fulfilled
-  delayed.end(promise);
-});
 
 router.get("/", iotaController.get);
 
-// router.post("/purchaseStream", iotaController.purchaseStream);
+router.post("/purchaseStream", iotaController.purchaseStream);
 
 
 module.exports = router;
