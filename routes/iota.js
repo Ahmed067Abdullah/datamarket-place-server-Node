ad63795d4ab988bb4dd255d2f67ffaf52e4a1774
Ahmed Abdullah <ahmed@sudofy.com>
2020-04-02 10:06:40 +0500

    asd

--- routes/iota.js
+++ routes/iota.js
@@ -4,14 +4,56 @@ const DelayedResponse = require('http-delayed-response');
 
 const router = express.Router();
 
-router.use(function (req, res) {
+router.use(function (req, res, next) {
+  const space = ' ';
+  let isFinished = false;
+  let isDataSent = false;
 
-    console.log('here');
-    var delayed = new DelayedResponse(req, res);
-    delayed.wait();
-    var promise = iotaController.purchaseStream();
-    // will eventually end when the promise is fulfilled
-    delayed.end(promise);
+  // Only extend the timeout for API requests
+  if (!req.url.includes('/purchaseStream')) {
+    next();
+    return;
+  }
+
+  res.once('finish', () => {
+    isFinished = true;
+  });
+
+  res.once('end', () => {
+    isFinished = true;
+  });
+
+  res.once('close', () => {
+    isFinished = true;
+  });
+
+  res.on('data', (data) => {
+    // Look for something other than our blank space to indicate that real
+    // data is now being sent back to the client.
+    if (data !== space) {
+      isDataSent = true;
+    }
+  });
+
+  const waitAndSend = () => {
+    setTimeout(() => {
+      // If the response hasn't finished and hasn't sent any data back....
+      if (!isFinished && !isDataSent) {
+        // Need to write the status code/headers if they haven't been sent yet.
+        if (!res.headersSent) {
+          res.writeHead(202);
+        }
+
+        res.write(space);
+
+        // Wait another 15 seconds
+        waitAndSend();
+      }
+    }, 15000);
+  };
+
+  waitAndSend();
+  next();
 });
 
 router.get("/", iotaController.get);
